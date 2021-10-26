931528d99c8713612a9387771820f6b53b1c47ca
tombrennan06 <website@ozultimate.com>
2021-10-27 09:29:17 +1100

    Add aerial layer and opacity slider

--- js/map.js
+++ js/map.js
@@ -1,8 +1,12 @@
+var lpi_nsw_imagery = L.tileLayer('http://maps.six.nsw.gov.au/arcgis/rest/services/public/NSW_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  maxZoom: 21,
+  attribution: '&copy; Land and Property Information 2016'
+});
 var lpi_nsw_topo_map = L.tileLayer('http://maps.six.nsw.gov.au/arcgis/rest/services/public/NSW_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
   maxZoom: 21,
   maxNativeZoom: 16,
   attribution: '&copy; Land and Property Information 2016',
-  opacity: 1.0
+  opacity: 0.5
 });
 
 var lat = -33.7067; //katoomba
@@ -10,9 +14,12 @@ var lng = 150.3156; //katoomba
 var map = L.map('map', {
   center: [lat, lng], 
   zoom: 13, 
-  layers: [lpi_nsw_topo_map],
+  layers: [lpi_nsw_imagery, lpi_nsw_topo_map],
   zoomControl: true
 });
 
+var opacitySlider = new L.Control.opacitySlider();
+map.addControl(opacitySlider);
+opacitySlider.setOpacityLayer(lpi_nsw_topo_map);
 L.control.scale({metric: true, imperial: false}).addTo(map);
 L.control.mouseCoordinate({utm:true,utmref:false}).addTo(map);
