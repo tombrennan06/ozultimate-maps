91f866dfd3c53f2eb0a0f450b259c5bfda48f415
tombrennan06 <website@ozultimate.com>
2021-10-27 15:28:37 +1100

    Edit labels

--- js/map.js
+++ js/map.js
@@ -2,6 +2,7 @@ var params = {};
 window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
   params[key] = value;
 });
+var init = {"lat": -33.7067, "lng": 150.3156, "zoom": 13 } //katoomba
 
 var lpi_nsw_imagery = L.tileLayer('http://maps.six.nsw.gov.au/arcgis/rest/services/public/NSW_Imagery/MapServer/tile/{z}/{y}/{x}', {
   maxZoom: 21,
@@ -14,13 +15,33 @@ var lpi_nsw_topo_map = L.tileLayer('http://maps.six.nsw.gov.au/arcgis/rest/servi
   opacity: 0.7
 });
 
-var lat = params.lat || -33.7067; //katoomba
-var lng = params.lng || 150.3156; //katoomba
+var lat = params.lat || init.lat;
+var lng = params.lng || init.lng;
 var map = L.map('map', {
   center: [lat, lng], 
-  zoom: params.zoom || 13, 
+  zoom: params.zoom || init.zoom, 
   layers: [lpi_nsw_imagery, lpi_nsw_topo_map],
   zoomControl: true
+});
+
+//-----------------
+//From
+//http://stackoverflow.com/questions/31221088/how-to-calculate-the-distance-of-a-polyline-in-leaflet-like-geojson-io
+L.GeometryUtil = L.extend(L.GeometryUtil || {}, {
+    getDistance: function(latlngs, system) {
+        // distance in meters
+        var mDistance = 0,
+            length = latlngs.length;
+        for (var i = 1; i < length; i++) {
+            mDistance += latlngs[i].distanceTo(latlngs[i - 1]);
+        }
+        // optional
+        if (system === 'imperial') {
+            return mDistance / 1609.34;
+        } else {
+            return mDistance / 1000;
+        }
+	}
 });
 
 var opacitySlider = new L.Control.opacitySlider();
@@ -31,34 +52,57 @@ L.control.mouseCoordinateNSW({utm:true,nswmap:true}).addTo(map);
 
 //-----------------
 //Leaflet.FileLayer
-var style = {color:'red', opacity: 1.0, fillOpacity: 1.0, weight: 2, clickable: false};
+var style = {color:'red', opacity: 1.0, fillOpacity: 1.0, weight: 2, clickable: true};
 L.Control.FileLayerLoad.LABEL = '<i class="fa fa-folder-open"></i>';
 L.Control.fileLayerLoad({
-    fitBounds: true,
-    layerOptions: {style: style,
-                   pointToLayer: function (data, latlng) {
-                      return L.marker(latlng);
-                   }
-                   , onEachFeature: function (feature, layer) {layer.bindPopup(feature.properties.name);}
-                   },
+  fitBounds: true,
+  layerOptions: {
+    style: style,
+    pointToLayer: function (data, latlng) {
+      return L.marker(latlng);
+    }
+    ,onEachFeature: onEachFeature
+  }
 }).addTo(map);
 
 // -----------
 // Leaflet.Draw
 // Initialise the FeatureGroup to store editable layers
-var drawnItems = new L.FeatureGroup();
+//var drawnItems = new L.FeatureGroup();
+var drawnItems = new L.GeoJSON();
 map.addLayer(drawnItems);
 
-// Initialise the draw control and pass it the FeatureGroup of editable layers
-var drawControl = new L.Control.Draw({
-    edit: {
-        featureGroup: drawnItems
+var drawOptions = {
+  draw: {
+    polyline: {
+      shapeOptions: {
+        color: 'red',
+        weight: 2,
+        opacity: 1
+      }
     },
-    draw: {
-      circle: false,
-      rectangle: false
-    }
-});
+    polygon: {
+      allowIntersection: false, // Restricts shapes to simple polygons
+      drawError: {
+        color: '#e1e100', // Color the shape will turn when intersects
+        message: 'Polygon can\'t overlap' // Message that will show when intersect
+      },
+      shapeOptions: {
+        color: 'red',
+        weight: 2
+      }
+    },
+    circle: false, 
+    rectangle: false, 
+  },
+  edit: {
+    featureGroup: drawnItems
+  }  
+}
+
+
+// Initialise the draw control and pass it the FeatureGroup of editable layers
+var drawControl = new L.Control.Draw(drawOptions);
 map.addControl(drawControl);
 
 var getShapeType = function(layer) {
@@ -70,21 +114,49 @@ var getShapeType = function(layer) {
 };
 
 map.on('draw:created', function(e) {
+  var measure = '';
   if (e.layerType === 'polygon') {
-    var area = L.GeometryUtil.geodesicArea(e.layer.getLatLngs());
-    e.layer.bindPopup((area / 1000000).toFixed(2) + ' km<sup>2</sup>');
+    measure = (L.GeometryUtil.geodesicArea(e.layer.getLatLngs()) / 1000000).toFixed(2) + ' km<sup>2</sup>';
   }
+  if (e.layerType === 'polyline') {
+    measure = L.GeometryUtil.getDistance(e.layer.getLatLngs()).toFixed(2) + ' km';
+  }
+  var input = L.DomUtil.create('input', 'my-input');
+  input.value = '';
+  e.layer.feature = {"properties":{"name":""}};
+  e.layer.feature.type = "Feature";
+  if (e.layerType === 'marker') {e.layer.feature.geometry = {"type":"Point"}};
+  if (e.layerType === 'polyline') {e.layer.feature.geometry = {"type":"LineString"}};
+  if (e.layerType === 'polygon') {e.layer.feature.geometry = {"type":"Polygon"}};
+  L.DomEvent.addListener(input, 'change', function () {
+    e.layer.feature.properties.name = input.value;
+	if (e.layer.feature.properties.name) {e.layer.bindLabel(e.layer.feature.properties.name + ' - ' + measure)}
+	else {e.layer.bindLabel(measure)};
+  });
+  L.DomEvent.addListener(input, 'keydown', function (v) {
+    if (v.keyCode == 13) {e.layer.closePopup();} //close on enter
+  });
+  e.layer.bindPopup(input);
+  if (e.layer.feature.properties.name) {e.layer.bindLabel(e.layer.feature.properties.name + ' - ' + measure)}
+  else {e.layer.bindLabel(measure)};
   drawnItems.addLayer(e.layer);
-  e.layer.openPopup();
+  if (e.layerType === 'marker') {
+    e.layer.openPopup();
+    input.focus();
+  }
 });
 
 map.on('draw:edited', function(e) {
   var layers = e.layers;
   layers.eachLayer(function (layer) {
     if (getShapeType(layer) === 'polygon') {
-      var area = L.GeometryUtil.geodesicArea(layer.getLatLngs())
-      layer.bindPopup((area / 1000000).toFixed(2) + ' km<sup>2</sup>');
+      measure = ' - ' + ((L.GeometryUtil.geodesicArea(layer.getLatLngs()) / 1000000).toFixed(2) + ' km<sup>2</sup>');
     }
+    if (getShapeType(layer) === 'polyline') {
+      measure = L.GeometryUtil.getDistance(layer.getLatLngs()).toFixed(2) + ' km';
+    }
+	if (layer.feature.properties.name) {layer.bindLabel(layer.feature.properties.name + ' - ' + measure)}
+	else {layer.bindLabel(measure)};
   });
 });
 
@@ -148,17 +220,21 @@ L.Control.Save = L.Control.extend({
     }
 });
 
-var saveKML = new L.Control.Save(drawnItems, {exportType: 'kml', label: '<span class="fa-stack">\n' +
-'    <i class="fa fa-save fa-pull-left"></i>\n'+
-'    <i class="fa fa-stack-1x">K</i>\n'+
-'</span>'});
-map.addControl(saveKML);
 var saveGPX = new L.Control.Save(drawnItems, {exportType: 'gpx', label: '<span class="fa-stack">\n' +
 '    <i class="fa fa-save fa-pull-left"></i>\n'+
 '    <i class="fa fa-stack-1x">G</i>\n'+
 '</span>'});
 map.addControl(saveGPX);
-
+var saveKML = new L.Control.Save(drawnItems, {exportType: 'kml', label: '<span class="fa-stack">\n' +
+'    <i class="fa fa-save fa-pull-left"></i>\n'+
+'    <i class="fa fa-stack-1x">K</i>\n'+
+'</span>'});
+map.addControl(saveKML);
+var saveGeoJSON = new L.Control.Save(drawnItems, {exportType: 'geojson', label: '<span class="fa-stack">\n' +
+'    <i class="fa fa-save fa-pull-left"></i>\n'+
+'    <i class="fa fa-stack-1x">G</i>\n'+
+'</span>'});
+map.addControl(saveGeoJSON);
 // Leaflet.Permalink 
 // Allows sharing of a permanent link. 
 // Requires 
@@ -222,16 +298,68 @@ L.Control.Permalink = L.Control.extend({
 var shareLink = new L.Control.Permalink(drawnItems, {label: '<i class="fa fa-link"></i>'});
 map.addControl(shareLink);
 
-// Add previously saved GeoJSON to map
+// Add previously saved GeoJSON or loaded file to map, and allow editing?
 function onEachFeature(feature, layer) {
+  var input = L.DomUtil.create('input', 'my-input');
   if (feature.properties && feature.properties.name) {
+    input.value = feature.properties.name;
+  } else {feature.properties.name = ''}
+  L.DomEvent.addListener(input, 'change', function () {
+    feature.properties.name = input.value;
     layer.bindLabel(feature.properties.name);
-  }
+  });
+  L.DomEvent.addListener(input, 'keydown', function (v) {
+    if (v.keyCode == 13) {layer.closePopup();}
+  });
+  layer.bindPopup(input);
+  layer.bindLabel(feature.properties.name);
+  drawnItems.addLayer(layer); //dodgy adding to global?
 }
 if (params.id) {
-  var geojsonLayer = new L.geoJson.ajax('data/' + params.id + '.geojson', { onEachFeature: onEachFeature } );
+  var geojsonLayer = new L.geoJson.ajax('data/' + params.id + '.geojson', { onEachFeature: onEachFeature, color: 'red', weight: 2, opacity: 1 } );
   geojsonLayer.on('data:progress', function(e) {
     map.fitBounds(geojsonLayer.getBounds(),{ padding: [50,50], maxZoom: 16 });
   });
   geojsonLayer.addTo(map);
+}
+
+// from https://github.com/mapbox/geojson-flatten/blob/master/index.js
+
+function flatten(gj) {
+    switch ((gj && gj.type) || null) {
+        case 'FeatureCollection':
+            gj.features = gj.features.reduce(function(mem, feature) {
+                return mem.concat(flatten(feature));
+            }, []);
+            return gj;
+        case 'Feature':
+            if (!gj.geometry) return gj;
+            return flatten(gj.geometry).map(function(geom) {
+                return {
+                    type: 'Feature',
+                    properties: JSON.parse(JSON.stringify(gj.properties)),
+                    geometry: geom
+                };
+            });
+        case 'MultiPoint':
+            return gj.coordinates.map(function(_) {
+                return { type: 'Point', coordinates: _ };
+            });
+        case 'MultiPolygon':
+            return gj.coordinates.map(function(_) {
+                return { type: 'Polygon', coordinates: _ };
+            });
+        case 'MultiLineString':
+            return gj.coordinates.map(function(_) {
+                return { type: 'LineString', coordinates: _ };
+            });
+        case 'GeometryCollection':
+            return gj.geometries.map(flatten).reduce(function(memo, geoms) {
+                return memo.concat(geoms);
+            }, []);
+        case 'Point':
+        case 'Polygon':
+        case 'LineString':
+            return [gj];
+    }
 }
--- save_geojson.php
+++ save_geojson.php
@@ -1,31 +1,17 @@
 <?php
-	$json = $_POST['json'];
-	$err = false; 
-	/* sanity check */
-	if (json_decode($json) != null) {
-		if (is_numeric($_POST['id'])) {
-			$id = $_POST['id'];
-			if (!($file = fopen('data/'.$id.'.geojson','w+'))) {
-				$err = true;
-				$message = 'Failed to open file';
-			}
-			fwrite($file, $json);
-			fclose($file);
-		}
-		else { 
-			$err = true; 
-			$message = 'Invalid id';
-		}
-	}
-	else
-	{
-		$err = true;
-		$message = 'Invalid JSON data';
-	}
-	if ($err)
-	{
-	header('HTTP/1.1 500 Internal Server Booboo');
-	header('Content-Type: application/json; charset=UTF-8');
-	die(json_encode(array('message' => 'ERROR', 'code' => $message)));
-	}
+   $json = $_POST['json'];
+
+   /* sanity check */
+   if (json_decode($json) != null) {
+		 if (is_numeric($_POST['id'])) {
+			 $id = $_POST['id'];
+			 $file = fopen('data/'.$id.'.geojson','w+');
+			 fwrite($file, $json);
+			 fclose($file);
+		 }
+   }
+   else
+   {
+     // user has posted invalid JSON, handle the error 
+   }
 ?>
