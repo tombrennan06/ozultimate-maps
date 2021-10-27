d888aac7447f530fb2837953b4d7b55818cbf2d0
tombrennan06 <website@ozultimate.com>
2021-10-27 11:42:34 +1100

    Series 1&2 map layer

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var lpi_nsw_imagery = L.tileLayer('http://maps.six.nsw.gov.au/arcgis/rest/services/public/NSW_Imagery/MapServer/tile/{z}/{y}/{x}', {
+var lpi_nsw_topo_map_s1 = L.tileLayer('http://maps.six.nsw.gov.au/arcgis/rest/services/sixmaps/LPITopoMap_S1/MapServer/tile/{z}/{y}/{x}', {
   maxZoom: 21,
   attribution: '&copy; Land and Property Information 2016'
 });
@@ -14,7 +14,7 @@ var lng = 150.3156; //katoomba
 var map = L.map('map', {
   center: [lat, lng], 
   zoom: 13, 
-  layers: [lpi_nsw_imagery, lpi_nsw_topo_map],
+  layers: [lpi_nsw_topo_map_s1, lpi_nsw_topo_map],
   zoomControl: true
 });
 
